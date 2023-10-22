faad885013a71c87faa09d86444b36604c302dd5
andres esteban ruiz <140468504+andresEstebanRuiz@users.noreply.github.comse>
2023-10-22 12:15:22 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+import phrases from "./utils/phrases.json"
+import getRandomFromArr from"./services/getRandomFromArr"
+import Phrase from './components/phrase'
+import ButtonPhrase from './components/ButtonPhrase'
+import bgArr from "./utils/bgArr"
+
+
+
+function App() {
+
+  const quote = getRandomFromArr(phrases)
+
+    const [phraseRandom, setPhraseRandom] = useState (quote)
+    const [bgApp, setBgApp]=useState(getRandomFromArr(bgArr))
+
+    const objStyle ={
+      backgroundImage: `url("../fondo${bgApp}.jpg")`
+    }
+  
+  return (
+    <div style={objStyle} className='app'>
+     
+     <h1 className='app__title'>Galleta de la fortuna</h1>
+
+     <article className='app__card'>
+      <Phrase
+        phraseRandom = {phraseRandom}
+      />
+      <ButtonPhrase
+        setPhraseRandom={setPhraseRandom}
+        setBgApp={setBgApp}
+      />
+     </article>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Phrase.jsx
@@ -0,0 +1,10 @@
+const Phrase = ({phraseRandom}) => {
+    
+    return (
+     
+          <p className="app__phrase">{phraseRandom.phrase}</p>
+      
+    )
+  }
+  
+  export default Phrase
